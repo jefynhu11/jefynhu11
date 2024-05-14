c76f022de6a59fae3c643839ade2071215a39912
jefynhu11 <38334949+jefynhu11@users.noreply.github.com>
2024-05-14 15:14:24 -0300

    Update Skills.vue

--- components/Skills.vue
+++ components/Skills.vue
@@ -2,27 +2,14 @@
     <section class="flex flex-col gap-y-3">
         <h2 class="text-2xl font-bold">Skills</h2>
         <div class="flex flex-wrap gap-1">
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">PHP</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Laravel</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Nuxt.js</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Javascript</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Typescript</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Java</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Python</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Robot Framework</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Cypress</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Playwright</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Nightwatch.js</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Selenium</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Appium</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Cucumber</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">MySQL</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Git</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">SikuliX</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">CSharp</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">REST Assured</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">SAP HANA</div>
-            <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Jira</div>
             <div class="bg-gray-800 px-2 py-0.5 border rounded-md text-white text-xs font-semibold font-mono">Trello</div>
         </div>
     </section>
