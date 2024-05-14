fc7bc882dc242d3a4b1edbc174d4f886c18285ed
jefynhu11 <38334949+jefynhu11@users.noreply.github.com>
2024-05-14 15:12:00 -0300

    Update Me.vue

--- components/Me.vue
+++ components/Me.vue
@@ -1,34 +1,34 @@
 <template>
     <div class="flex items-center justify-between gap-8">
         <div class="flex-1 space-y-1.5">
-            <h1 class="text-3xl font-bold">Cleverson Sampaio</h1>
-            <p class="text-pretty font-mono text-sm text-gray-500">Senior QA Automation Engineer</p>
+            <h1 class="text-3xl font-bold">Jeferson Lopes Eugenio</h1>
+            <p class="text-pretty font-mono text-sm text-gray-500">Develop I</p>
             <p class="text-pretty font-mono text-sm text-gray-500">
                 <a href="https://maps.app.goo.gl/nMFdjBFr85Hr3Fjj9" target="_blank" class="flex items-center gap-3">
                     <Icon name="bx:world" dynamic />
-                    Porto Alegre, RS - Brazil
+                    Canoas, RS - Brazil
                 </a>
             </p>
             <div class="flex flex-wrap gap-3">
-                <a href="mailto:cleverson@sampaio.dev.br" class="h-8 px-3 border rounded-sm flex items-center justify-between gap-3">
+                <a href="mailto:jefyx1@hotmail.com" class="h-8 px-3 border rounded-sm flex items-center justify-between gap-3">
                     <Icon name="bx:envelope" dynamic />
-                    <p class="text-pretty font-mono text-sm text-gray-500 hidden print:flex">cleverson@sampaio.dev.br</p>
+                    <p class="text-pretty font-mono text-sm text-gray-500 hidden print:flex">jefyx1@hotmail.com</p>
                 </a>
-                <a href="tel:+5551983109402" class="h-8 px-3 border rounded-sm flex items-center justify-between gap-3">
+                <a href="tel:+5551986868705" class="h-8 px-3 border rounded-sm flex items-center justify-between gap-3">
                     <Icon name="bx:phone" dynamic />
-                    <p class="text-pretty font-mono text-sm text-gray-500 hidden print:flex">+5551983109402</p>
+                    <p class="text-pretty font-mono text-sm text-gray-500 hidden print:flex">+5551986868705</p>
                 </a>
-                <a href="https://github.com/kriffx" target="_blank" class="h-8 px-3 border rounded-sm flex items-center justify-between gap-3">
+                <a href="https://github.com/jefynhu11" target="_blank" class="h-8 px-3 border rounded-sm flex items-center justify-between gap-3">
                     <Icon name="bx:bxl-github" dynamic />
-                    <p class="text-pretty font-mono text-sm text-gray-500 hidden print:flex">kriffx</p>
+                    <p class="text-pretty font-mono text-sm text-gray-500 hidden print:flex">jefynhu11</p>
                 </a>
-                <a href="https://www.linkedin.com/in/clvrsnsampaio" target="_blank" class="h-8 px-3 border rounded-sm flex items-center justify-between gap-3">
+                <a href="https://www.linkedin.com/in/jeferson-eugenio/" target="_blank" class="h-8 px-3 border rounded-sm flex items-center justify-between gap-3">
                     <Icon name="bx:bxl-linkedin-square" dynamic />
-                    <p class="text-pretty font-mono text-sm text-gray-500 hidden print:flex">in/clvrsnsampaio</p>
+                    <p class="text-pretty font-mono text-sm text-gray-500 hidden print:flex">in/jeferson-eugenio/</p>
                 </a>
             </div>
         </div>
-        <img src="https://avatars.githubusercontent.com/u/146900991?v=4" alt="Cleverson Sampaio" class="w-28 rounded-full" />
+        <img src="https://avatars.githubusercontent.com/u/38334949?v=4" alt="Jeferson Eugenio" class="w-28 rounded-full" />
     </div>
 </template>
 
