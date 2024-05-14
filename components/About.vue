96e1c7731dac0ad92305849ebac8ce49b6e64c7b
jefynhu11 <38334949+jefynhu11@users.noreply.github.com>
2024-05-14 14:27:18 -0300

    Update About.vue

--- components/About.vue
+++ components/About.vue
@@ -2,9 +2,8 @@
     <section class="flex flex-col gap-y-3">
         <h1 class="text-2xl font-bold">About</h1>
         <p class="text-pretty font-mono text-sm text-gray-500">
-            As a Senior QA Automation Engineer, I successfully participated in several projects making a difference. 
-            Currently I work mainly with automated testing/web and mobile development. 
-            I have more than 6 years of experience working remotely with companies in Portugal and Brazil.
+            I currently work in academia with automated testing/web development.
+            I have 5 years of experience working remotely with DB company.
         </p>
     </section>
 </template>
